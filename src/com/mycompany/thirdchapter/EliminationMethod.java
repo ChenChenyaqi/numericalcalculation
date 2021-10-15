676e539707857b539ab961351346093a081bfce6
chenyaqi <15229293783@163.com>
2021-10-15 21:24:35 +0800

    高斯全主元消元法求解线性方程组

---
+++ src/com/mycompany/thirdchapter/EliminationMethod.java
@@ -0,0 +1,117 @@
+package com.mycompany.thirdchapter;
+
+/**
+ * 消元法解线性方程组（求唯一解，即要求系数矩阵的行列式不为0）
+ * 高斯全主元消元法 和 高斯列主元消元法
+ *
+ * @author Chen Yaqi
+ * @version 1.0
+ */
+public class EliminationMethod {
+    // 记录线性方程组 n 个未知数的位置，针对 进行行交换或列交换时，使其乱序问题
+    private static int[] rootIndex;
+
+    /**
+     * 用高斯全主元消元法求线性方程组的唯一解
+     *
+     * @param matrix 线性方程组的系数矩阵
+     * @param b      线性方程组等式右端的值, AX = b
+     * @return 唯一解
+     */
+    public static double[] totalElementEliminate(double[][] matrix, double[] b) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0 || matrix.length != matrix[0].length) {
+            System.out.println("输入的系数矩阵不合法");
+            return null;
+        }
+        if (b == null || b.length != matrix.length) {
+            System.out.println("输入的b不合法");
+            return null;
+        }
+        if (MatrixUtils.det(matrix) == 0) {
+            System.out.println("行列式值为0，无法求解");
+            return null;
+        }
+        // 系数矩阵的阶
+        int n = matrix.length;
+        // 增广矩阵
+        double[][] augmentedMatrix = new double[n][n + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (j == n) {
+                    augmentedMatrix[i][j] = b[i];
+                    continue;
+                }
+                augmentedMatrix[i][j] = matrix[i][j];
+            }
+        }
+        // 初始化rootIndex
+        rootIndex = new int[n];
+        for (int i = 0; i < n; i++) {
+            // x1 = 1, x2 = 2, x3 = 4 ...
+            rootIndex[i] = i + 1;
+        }
+        // 开始消元
+        // 用第 i 行, 消 i + 1 ~ n - 1 行
+        for (int i = 0; i < n - 1; i++) {
+            // 先进行行交换或列交换，使[i,i]位置上的元素绝对值最大
+            change(augmentedMatrix, i);
+            for (int j = i + 1; j < n; j++) {
+                MatrixUtils.eliminate(augmentedMatrix, i, j);
+            }
+        }
+        // 求解算法，倒着往回求
+        double[] noOrderRoots = new double[n];
+        noOrderRoots[n - 1] =  augmentedMatrix[n-1][n] / augmentedMatrix[n - 1][n - 1];
+        for (int i = n - 2; i >= 0 ; i--) {
+            for (int j = n - 1; j > i ; j--) {
+                augmentedMatrix[i][n] -= noOrderRoots[j] * augmentedMatrix[i][j];
+            }
+            noOrderRoots[i] = augmentedMatrix[i][n] / augmentedMatrix[i][i];
+        }
+        // 根据rootIndex中此时各个未知量的相对次序，对解排序，返回排好序的解
+        double[] orderRoots = new double[n];
+        for (int i = 0; i < n; i++) {
+            orderRoots[rootIndex[i] - 1] = noOrderRoots[i];
+        }
+        return orderRoots;
+    }
+
+    /**
+     * 对矩阵进行行列交换，使得每次消元前，主对角元的值都是相对较大的（绝对值大）
+     *
+     * @param matrix 矩阵matrix
+     * @param i      从i行i列到末尾的这个子阵
+     */
+    private static void change(double[][] matrix, int i) {
+        // 得到[i,i]之后最大数的下标
+        int[] maxNumIndex = MatrixUtils.getMaxNumIndex(matrix, i);
+        // 如果最大数就是matrix[i][i]本身，则不交换
+        if (maxNumIndex[0] == i && maxNumIndex[1] == i) {
+            return;
+        }
+        // 如果列不相等
+        if (maxNumIndex[1] != i) {
+            // 交换列
+            for (int j = 0; j < matrix.length; j++) {
+                double temp = matrix[j][i];
+                matrix[j][i] = matrix[j][maxNumIndex[1]];
+                matrix[j][maxNumIndex[1]] = temp;
+            }
+            // 交换rootIndex里xn 和 xm 的位置
+            int temp = rootIndex[i];
+            rootIndex[i] = rootIndex[maxNumIndex[1]];
+            rootIndex[maxNumIndex[1]] = temp;
+        }
+        // 如果行不相等
+        if (maxNumIndex[0] != i) {
+            // 交换行
+            for (int j = 0; j < matrix[0].length; j++) {
+                double temp = matrix[i][j];
+                matrix[i][j] = matrix[maxNumIndex[0]][j];
+                matrix[maxNumIndex[0]][j] = temp;
+            }
+        }
+    }
+
+
+}
---
+++ src/com/mycompany/thirdchapter/TestEliminationMethod.java
@@ -0,0 +1,41 @@
+package com.mycompany.thirdchapter;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * 测试消元法求解线性方程组算法
+ * @author Chen Yaqi
+ * @version 1.0
+ */
+public class TestEliminationMethod {
+    // 测试高斯全主元消元法求解线性方程组
+    @Test
+    public void testTotalEliminate(){
+        // 系数矩阵
+        double[][] matrix = new double[][]{
+                {30.5,6.5,7,-55},
+                {65,2.36,10,615},
+                {5,56,5.5,102.9,165},
+                {-12,0,0,15,61}
+        };
+        double[] b = new double[]{1,4,18.5,-10};
+        double[] roots = EliminationMethod.totalElementEliminate(matrix, b);
+        System.out.println("线性方程组 AX=b 求解测试结果如下：");
+        System.out.println("线性方程组系数矩阵A为：");
+        MatrixUtils.show(matrix);
+        System.out.println("解为：");
+        System.out.println(Arrays.toString(roots));
+        System.out.println("带入线性方程组算y：");
+        double[] y = new double[roots.length];
+        for (int i = 0; i < roots.length; i++) {
+            for (int j = 0; j < roots.length; j++) {
+                y[i] += roots[j] * matrix[i][j];
+            }
+        }
+        System.out.println(Arrays.toString(y));
+        System.out.println("b为：");
+        System.out.println(Arrays.toString(b));
+    }
+}
